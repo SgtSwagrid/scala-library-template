573d0da2cdae00889069e5fe1be28914ab80f09e
SgtSwagrid <SgtSwagrid@users.noreply.github.com>
2026-04-19 13:16:05 +0000

    deploy: 287bf1bf21d29a0fa60672408069e4861b49ddbf

---
+++ scripts/searchData.js
@@ -0,0 +1,4 @@
+pages = [{"l":"index.html#","e":false,"i":"","n":"scala-library-template","t":"scala-library-template","d":"","k":"static","x":""},
+{"l":"com/alecdorrington.html#","e":false,"i":"","n":"com.alecdorrington","t":"com.alecdorrington","d":"","k":"package","x":""},
+{"l":"com/alecdorrington/Example$.html#","e":false,"i":"","n":"Example","t":"Example","d":"com.alecdorrington","k":"object","x":""},
+{"l":"com/alecdorrington/Example$.html#hello-0","e":false,"i":"","n":"hello","t":"hello: String","d":"com.alecdorrington.Example","k":"def","x":""}];
